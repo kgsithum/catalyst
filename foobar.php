0d8b2db96604eb2561d3bbb0ff6b3f18d4e98b9f
Sithum Amarasinghe <sithum@Sithums-MacBook-Pro.local>
2017-08-03 23:48:14 +1200

    add seperattor

--- foobar.php
+++ foobar.php
@@ -1,17 +1,22 @@
 <?php
 
 $print_str = "";
+$seperator = ",";
 
 for($i=1;$i<=100;$i++){
 
+  if($i == 100){
+    $seperator = "";
+  }
+
   if(($i%3 == 0) && ($i%5 == 0)){
-    $print_str .= "foobar";
+    $print_str .= "foobar".$seperator;
   }else if($i%3 == 0){
-    $print_str .= "foo";
+    $print_str .= "foo".$seperator;
   }else if($i%5 == 0){
-    $print_str .= "bar";
+    $print_str .= "bar".$seperator;
   }else{
-    $print_str .= $i;
+    $print_str .= $i.$seperator;
   }
 
 }
