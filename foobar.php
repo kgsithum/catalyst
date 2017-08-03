4e931d894b6820f65ab3a9ead34bfe6868fe1859
Sithum Amarasinghe <sithum@Sithums-MacBook-Pro.local>
2017-08-03 23:44:15 +1200

    add foobar logic

--- foobar.php
+++ foobar.php
@@ -4,7 +4,15 @@ $print_str = "";
 
 for($i=1;$i<=100;$i++){
 
+  if(($i%3 == 0) && ($i%5 == 0)){
+    $print_str .= "foobar";
+  }else if($i%3 == 0){
+    $print_str .= "foo";
+  }else if($i%5 == 0){
+    $print_str .= "bar";
+  }else{
     $print_str .= $i;
+  }
 
 }
 echo "$print_str\n\n";
