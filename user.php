e54c5711bac53899fbb6ae0373a302f2f8cbb3e2
Sithum Amarasinghe <kgsithum@gmail.com>
2017-08-07 18:39:18 +1200

    accept derectives -u, -p, -h and create db connection

--- user.php
+++ user.php
@@ -13,10 +13,23 @@ class User {
   private $db_connection;
 
 
-  public function __construct() {
+  public function __construct($array_params=array()) {
 
+    //check params
+    if(!isset($array_params['DB_HOST'])){
+      //set default host
+      $array_params['DB_HOST'] = DB_HOST;
+    }
+    if(!isset($array_params['DB_USERNAME'])){
+      //set default host
+      $array_params['DB_USERNAME'] = DB_USERNAME;
+    }
+    if(!isset($array_params['DB_PASSWORD'])){
+      //set default host
+      $array_params['DB_PASSWORD'] = DB_PASSWORD;
+    }
     // establish connection
-    if(!$this->db_connection = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD)) {
+    if(!$this->db_connection = mysqli_connect($array_params['DB_HOST'], $array_params['DB_USERNAME'], $array_params['DB_PASSWORD'])) {
       throw new Exception('Error connecting to MySQL: '.mysqli_error());
     }
 
@@ -34,7 +47,7 @@ class User {
 
     // Fetch all
     return mysqli_fetch_all($result,MYSQLI_ASSOC);
-    
+
   }
 
 
--- user_upload.php
+++ user_upload.php
@@ -4,9 +4,41 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include("user.php");
 
-$user_obj = new User();
-$str = $user_obj->test();
-print_r($str);
+//print_r($argc);
+
+//check arguments
+if($argc > 1){
+  echo "args\n";
+
+  //create user object. create mysql connection with params
+  $params = array();
+  //get -u if exist
+  if($user_index = array_search("-u",$argv)){
+    $db_user = $argv[$user_index + 1];
+    $params['DB_USERNAME'] = $db_user;
+  }
+  //get -p if exist
+  if($pass_index = array_search("-p",$argv)){
+    $db_pass = $argv[$pass_index + 1];
+    $params['DB_PASSWORD'] = $db_pass;
+  }
+  //get -h if exist
+  if($host_index = array_search("-h",$argv)){
+    $db_host = $argv[$host_index + 1];
+    $params['DB_HOST'] = $db_host;
+  }
+
+  //create user object
+  $user_obj = new User($params);
+  $str = $user_obj->test();
+  print_r($str);
+  
+}else{
+  echo "no args\n";
+}
+
+
+
 
 
 
