f5d45be25d40f797a5e05eda9a81ca7d8bc5b2c5
Sithum Amarasinghe <kgsithum@gmail.com>
2017-08-08 14:57:39 +1200

    implemented help derective

--- user_upload.php
+++ user_upload.php
@@ -153,7 +153,20 @@ if($argc > 1){
 
       //process --help derective
       case '--help':
-        echo "help called.\n";
+
+        $str = "
+                • --file [csv file name] – this is the name of the CSV to be parsed\n
+                • --create_table – this will cause the MySQL users table to be built (and no further action will be taken)\n
+                • --dry_run – this will be used with the --file directive in the instance that we want to run the
+                script but not insert into the DB. All other functions will be executed, but the database won\'t
+                be altered.\n
+                • -u – MySQL username\n
+                • -p – MySQL password\n
+                • -h – MySQL host\n
+                • --help – which will output the help list of directives with details.\n";
+
+        echo $str;
+
       break;
 
       default:
